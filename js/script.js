2a032e9b7f153fcebe9b55a99fd20e6d2ebae305
kamauvick <kamauvick@outlook.com>
2019-07-29 10:44:57 +0300

    Make changes to the js file.
    Refactor radio button code.

--- js/script.js
+++ js/script.js
@@ -6,12 +6,6 @@ $(function () {
         let crust = $("#crust option:selected").val();
         let topping = $("#toppings option:selected").val();
         let number = $("#number").val();
-        let delivery_mode = [];
-        $.each(("input[name ='delivery']:checked"), () => {
-            delivery_mode.push($(this).val());
-            let del_mode = JSON.parse(JSON.stringify(delivery_mode));
-            console.log(del_mode);
-        });
         let order = (f, s, c, t, n, d) => {
             return {
                 f,
