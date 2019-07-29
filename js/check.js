7f8cad0635e2d9d9d9bcc033ed1bcbfb69d8e420
kamauvick <kamauvick@outlook.com>
2019-07-29 17:25:35 +0300

    Add a subscribe alert to the page.

--- js/check.js
+++ js/check.js
@@ -1,3 +1,9 @@
-var price, flavour, size, crust, toppings, number;
-flavour = document.getElementsByClassName('.flavour').value;
+let size, email;
 size = document.getElementById('#size').value;
+email = document.getElementById('email').value;
+
+function subscribe(){
+    alert("Hello " + email + "Thank for reaching out");
+    console.log(email);
+}
+
